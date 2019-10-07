df8ccc6091afc4fa7d59449611b8252ec8e89f2c
brayvera <brayamveracruz@gmail.com>
2019-10-06 20:11:31 -0500

    new

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,75 @@
+     <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Admin</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i class="material-icons">settings_input_svideo</i>Escritorio</a></li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_balance</i>Departamento<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="adddepartment.php">Agregar departamento</a></li>
+                                <li><a href="managedepartments.php">Administrar departamento</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">book</i>Tipo de licencia<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addleavetype.php">Agregar tipo de licencia</a></li>
+                                <li><a href="manageleavetype.php">Administrar tipo de licencia</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">accessibility</i>Empleados<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addemployee.php">Agregar empleado</a></li>
+                                <li><a href="manageemployee.php">Administrar Empleado</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+   <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Gestionar Licencias<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="leaves.php">Todas las hojas </a></li>
+                                <li><a href="pending-leavehistory.php"> Hojas pendientes </a></li>
+                                <li><a href="approvedleave-history.php">Hojas aprobadas</a></li>
+                                  <li><a href="notapproved-leaves.php">Hojas no aprobadas</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+
+
+
+                        <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Desconectar</a>
+                            </li>  
+                 
+
+                 
+              
+                </ul>
+                   <div class="footer">
+                    <p class="copyright"><a href="https://www.facebook.com/sistemasundac/" target="blank">Ingenieria de Sistemas y computación</a></p>
+                
+                </div>
+                </div>
+            </aside>
